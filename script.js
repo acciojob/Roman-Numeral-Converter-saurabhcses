f4785cdc219be5ff9f1285a8ea89f2d7e42e28ea
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-10-03 14:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 function convertToRoman(num) {
-  if (num === 0) return ""; // no Roman numeral for 0
+  if (num === 0) return ""; // No Roman numeral for 0
 
-  // Symbols for Roman numerals including subtractive notation
-  const romanSymbols = [
-    ["C̄", 100000], ["X̄C̄", 90000], ["L̄X̄", 50000], ["X̄L̄", 40000], ["X̄", 10000], ["ĪX̄", 9000],
-    ["V̄", 5000], ["ĪV̄", 4000],
-    ["M", 1000], ["CM", 900], ["D", 500], ["CD", 400],
-    ["C", 100], ["XC", 90], ["L", 50], ["XL", 40],
-    ["X", 10], ["IX", 9], ["V", 5], ["IV", 4], ["I", 1]
+  const romanNumerals = [function convertToRoman(num) {
+    // Create a mapping of Roman numerals to their values
+    const romanNumerals = [
+        ['M', 1000],
+        ['D', 500],
+        ['C', 100],
+        ['L', 50],
+        ['X', 10],
+        ['V', 5],
+        ['I', 1]
+    ];
+
+    let result = '';
+
+    // Iterate over the romanNumerals array
+    for (let i = 0; i < romanNumerals.length; i++) {
+        // While the number is greater than the current value
+        while (num >= romanNumerals[i][1]) {
+            result += romanNumerals[i][0]; // Append the Roman numeral
+            num -= romanNumerals[i][1];    // Subtract the value from num
+        }
+    }
+
+    return result; // Return the final Roman numeral
+}
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
   ];
 
   let result = "";
 
-  for (const [symbol, value] of romanSymbols) {
+  for (let i = 0; i < romanNumerals.length; i++) {
+    const [symbol, value] = romanNumerals[i];
     while (num >= value) {
       result += symbol;
       num -= value;
@@ -20,10 +52,3 @@ function convertToRoman(num) {
   }
 
   return result;
-}
-
-// Examples:
-console.log(convertToRoman(14));   // XIV
-console.log(convertToRoman(798));  // DCCXCVIII
-console.log(convertToRoman(0));    // "" (empty string)
-console.log(convertToRoman(100000)); // C̄
